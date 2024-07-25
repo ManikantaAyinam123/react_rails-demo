e898ad7a669227965a313d1edaf9babbb6766ced
ManikantaAyinam123 <manikanta@actimize.in>
2024-07-25 10:11:39 +0530

    v

---
+++ app/javascript/components/App.js
@@ -0,0 +1,19 @@
+// App.js
+import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Home";
+import AboutUs from "./AboutUs";
+
+export default function App() {
+  return (
+
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+          <Route path="aboutus" element={<AboutUs/>} />
+        
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
