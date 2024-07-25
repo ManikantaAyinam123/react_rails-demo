1976fabdd8d0bd35db5a835dc1591346f800e7ae
ManikantaAyinam123 <manikanta@actimize.in>
2024-07-25 17:21:36 +0530

    build

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,11 +1,14 @@
-
+#!/usr/bin/env bash# exit on error
 set -o errexit
 
 # bundle install
 # bundle exec rails assets:precompile
 # bundle exec rails assets:clean
 bundle install
-bundle exec rails assets:precompile
-bundle exec rails assets:clean
-rails db:migrate
-bundle exec rails webpacker:install 
+ bundle exec rake assets:precompile
+  bundle exec rake assets:clean
+  ./bin/render-build.sh
+  ./bin/rails db:migrate
+   rails webpacker:install
+   bundle exec rails webpacker:install 
+   npm install
