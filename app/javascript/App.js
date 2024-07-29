d53be74e53139532ef30e66a00ccb8b8b710c712
ManikantaAyinam123 <manikanta@actimize.in>
2024-07-29 18:50:28 +0530

    changes in proc

--- app/javascript/App.js
+++ app/javascript/App.js
@@ -1,8 +1,8 @@
 // App.js
 import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "..components/Home";
-import AboutUs from "..components/AboutUs";
+import Home from "../components/Home";
+import AboutUs from "../components/AboutUs";
 
 export default function App() {
   return (
--- app/javascript/packs/index.jsx
+++ app/javascript/packs/index.jsx
@@ -1,15 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import PropTypes from 'prop-types'
-import App from '../App'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from '../App';
 
-
-
-
+// Ensure that the App component file is located at src/app/javascript/App.js
+// If it is located elsewhere, adjust the import path accordingly.
 
 document.addEventListener('DOMContentLoaded', () => {
+  // Create a new div element and append it to the body
+  const container = document.createElement('div');
+  document.body.appendChild(container);
+
+  // Render the App component into the newly created div
   ReactDOM.render(
     <App />,
-    document.body.appendChild(document.createElement('div')),
-  )
-})
+    container
+  );
+});
