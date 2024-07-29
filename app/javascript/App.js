1b727c418f35818447eb697ef9566411ab4f7ae8
ManikantaAyinam123 <manikanta@actimize.in>
2024-07-29 16:56:39 +0530

    changes in proc

---
+++ app/javascript/App.js
@@ -0,0 +1,19 @@
+// App.js
+import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "..components/Home";
+import AboutUs from "..components/AboutUs";
+
+export default function App() {
+  return (
+
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+          <Route path="aboutus" element={<AboutUs/>} />
+        
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
